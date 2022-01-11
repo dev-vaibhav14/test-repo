7e140c8397e0bb8c43653f34f978392c5b8d1c39
Vaibhav Mahajan <61781555+dev-vaibhav14@users.noreply.github.com>
2022-01-11 13:10:49 +0530

    third commit

---
+++ TestClass.java
@@ -0,0 +1,11 @@
+package com.java.ds.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is test class");
+
+	}
+
+}
